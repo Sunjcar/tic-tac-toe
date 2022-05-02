0e5c73fccb5fe1586234e8d4cf5601a5db508e91
Raymond Chen <raychen358@gmail.com>
2022-05-01 22:26:51 -0700

    Added in alternating X & O click inputsand delcares Winner if 3 in a row/col/diagonal

--- app.js
+++ app.js
@@ -1,33 +1,71 @@
 const gameBoard = (function(){
-    var gameboard = [];
+    const winCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
 
+    
+    let previousClick = "O"
+
+    //Cache Dom
+    const display = document.getElementById('display')
+    const restart = document.getElementById('restart')
     //DOM creation 
     const container = document.createElement('container');
     document.body.appendChild(container);
     function createBoard(){
         for (let i = 0; i<9; i++){
-            const div = document.createElement('div')
+            const div = document.createElement('div')    
             div.className = 'square'
-            container.appendChild(div);
+            container.appendChild(div);   
+            //bind Events
+            div.addEventListener('click',playerSelection, {once:true});
+            restart.addEventListener('click',resetGame)
         }
     }
-    //Cache Dom
-    const div = document.querySelectorAll('.square')
-    //Bind Events
 
-       
     //Render
     function render(){
         createBoard();
-        player1();
     }
     render();
 
-    function player1(){
-        div.textContent= "X"
+    function playerSelection(e){
+        if (e.target.textContent === false)
+            return;
+        if (previousClick === "X") {
+            e.target.textContent = "O"
+            previousClick = "O";
+            checkWin();
+        }else if (previousClick === "O"){
+            e.target.textContent = "X";
+            previousClick = "X";     
+            checkWin();
+        }
+       
+    }    
+    function checkWin(){ 
+    for (let i=0,n=winCombos.length; i<n; i++)
+    {
+        const squares = document.querySelectorAll('div.square')
+        let currentCombos = winCombos[i];
+        let value = squares[currentCombos[0]].textContent;
+        if (value !== '')
+        {
+            if (squares[currentCombos[1]].textContent == value && squares[currentCombos[2]].textContent == value)
+            {
+                display.textContent = "Winner";
+            }
+        }
     }
-
-    function player2(){
-        div.textContent= "O"
+}
+    function resetGame (){
+        location.reload();
     }
 })()
